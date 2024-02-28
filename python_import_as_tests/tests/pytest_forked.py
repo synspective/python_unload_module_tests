dec3d7a7c5ba69bcbc7883fc3c400c53b149859d
Florent Pastor <florentpastor@flpas-1.local>
2024-02-28 14:07:35 +0900

    Check how environment variable works

--- python_import_as_tests/tests/pytest_forked.py
+++ python_import_as_tests/tests/pytest_forked.py
@@ -1,11 +1,20 @@
 import pytest
+import os
 
 @pytest.mark.forked
 def test_one():
+    assert "TEST" not in os.environ
+    os.environ["TEST"] = "test_one"
+    assert os.environ["TEST"] == "test_one"
+
     from python_import_as_tests.my_package_main import setup
     assert setup() is False
 
 @pytest.mark.forked
 def test_two():
+    assert "TEST" not in os.environ
+    os.environ["TEST"] = "test_two"
+    assert os.environ["TEST"] == "test_two"
+
     from python_import_as_tests.my_package_main import setup
     assert setup() is False
